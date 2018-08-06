df23e303692e6a2e59c4e5668a11615f62b1725b
Norbert Nopper <nopper@ux3d.io>
2018-08-06 14:41:14 +0200

    Refactoring by splitting code (WIP)

--- addons/io_scene_gltf2/blender/export/gltf2_blender_export.py
+++ addons/io_scene_gltf2/blender/export/gltf2_blender_export.py
@@ -16,12 +16,11 @@
 # Imports
 #
 
-import json
-import struct
-
 import bpy
 
 from ...io.common.gltf2_io_debug import *
+
+from ...io.export.gltf2_io_export import *
 
 from .gltf2_blender_create import *
 from .gltf2_blender_filter import *
@@ -88,69 +87,7 @@ def save(operator,
 
     #
 
-    indent = None
-    separators = separators=(',', ':')
-
-    if export_settings['gltf_format'] == 'ASCII' and not export_settings['gltf_strip']:
-        indent = 4
-        # The comma is typically followed by a newline, so no trailing whitespace is needed on it.
-        separators = separators=(',', ' : ')
-
-    glTF_encoded = json.dumps(glTF, indent=indent, separators=separators, sort_keys=True, cls=BlenderEncoder)
-    
-    #
-
-    if export_settings['gltf_format'] == 'ASCII':
-        file = open(export_settings['gltf_filepath'], "w", encoding="utf8", newline="\n")
-        file.write(glTF_encoded)
-        file.write("\n")
-        file.close()
-        
-        binary = export_settings['gltf_binary']
-        if len(binary) > 0 and not export_settings['gltf_embed_buffers']:
-            file = open(export_settings['gltf_filedirectory'] + export_settings['gltf_binaryfilename'], "wb")
-            file.write(binary)
-            file.close()
-        
-    else:
-        file = open(export_settings['gltf_filepath'], "wb")
-
-        glTF_data = glTF_encoded.encode()
-        binary = export_settings['gltf_binary']
-
-        length_gtlf = len(glTF_data)
-        spaces_gltf = (4 - (length_gtlf & 3)) & 3
-        length_gtlf += spaces_gltf
-
-        length_bin = len(binary)
-        zeros_bin = (4 - (length_bin & 3)) & 3
-        length_bin += zeros_bin
-
-        length = 12 + 8 + length_gtlf
-        if length_bin > 0:
-            length += 8 + length_bin
-        
-        # Header (Version 2)
-        file.write('glTF'.encode())
-        file.write(struct.pack("I", 2))
-        file.write(struct.pack("I", length))
-        
-        # Chunk 0 (JSON)
-        file.write(struct.pack("I", length_gtlf)) 
-        file.write('JSON'.encode())
-        file.write(glTF_data)
-        for i in range(0, spaces_gltf):
-            file.write(' '.encode())
-
-        # Chunk 1 (BIN)
-        if length_bin > 0:
-            file.write(struct.pack("I", length_bin)) 
-            file.write('BIN\0'.encode())
-            file.write(binary)
-            for i in range(0, zeros_bin):
-                file.write('\0'.encode())
-            
-        file.close()
+    save_gltf(glTF, export_settings, BlenderEncoder)
         
     #
     
