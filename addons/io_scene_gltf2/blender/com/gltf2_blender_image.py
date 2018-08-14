9ae5f12f6e190d583aa6e055cb032c3781e0b099
Urs Hanselmann <hanselmann@ux3d.io>
2018-08-14 14:37:05 +0200

    Add image utility functions

---
+++ addons/io_scene_gltf2/blender/com/gltf2_blender_image.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2017 The Khronos Group Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# Imports
+#
+
+from ...io.com.gltf2_io_image import *
+
+def create_img_from_blender_image(blender_image):
+    """
+    Create a new image object using the given blender image.
+    Returns the created image object.
+    """
+    
+    if blender_image is None:
+        return None
+
+    return create_img_from_pixels(blender_image.size[0], blender_image.size[1], blender_image.pixels[:])
