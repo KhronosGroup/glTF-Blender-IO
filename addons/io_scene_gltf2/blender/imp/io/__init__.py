d9b85d373d7ccd970d22e3af6225ad9385eda088
Julien Duroure <julien.duroure@gmail.com>
2018-08-23 23:23:29 +0200

    Set bone length when there are in chain
    Use distance to point to detect if a bone is in chain or not

--- addons/io_scene_gltf2/blender/imp/io/__init__.py
+++ addons/io_scene_gltf2/blender/imp/io/__init__.py
@@ -227,6 +227,44 @@ class glTFImporter():
         for scene in self.other_scenes:
             scene.blender_create()
 
+        # Armature correction
+        # Try to detect bone chains, and set bone lengths
+        # To detect if a bone is in a chain, we try to detect if a bone head is aligned
+        # with parent_bone :
+        ##          Parent bone defined a line (between head & tail)
+        ##          Bone head defined a point
+        ##          Calcul of distance between point and line
+        ##          If < threshold --> In a chain
+        ## Based on an idea of @Menithal, but added alignement detection to avoid some bad cases
+
+        threshold = 0.001
+        for armobj in [obj for obj in bpy.data.objects if obj.type == "ARMATURE"]:
+            bpy.context.scene.objects.active = armobj
+            armature = armobj.data
+            bpy.ops.object.mode_set(mode="EDIT")
+            for bone in armature.edit_bones:
+                if bone.parent is None:
+                    continue
+
+                parent = bone.parent
+
+                # case where 2 bones are aligned (not in chain, same head)
+                if (bone.head - parent.head).length < threshold:
+                    continue
+
+                u = (parent.tail - parent.head).normalized()
+                point = bone.head
+                distance = ((point - parent.head).cross(u)).length / u.length
+                if distance < threshold:
+                    save_parent_direction = (parent.tail - parent.head).normalized().copy()
+                    save_parent_tail = parent.tail.copy()
+                    parent.tail = bone.head
+
+                    # case where 2 bones are aligned (not in chain, same head)
+                    # bone is no more is same direction
+                    if (parent.tail - parent.head).normalized().dot(save_parent_direction) < 0.9:
+                        parent.tail = save_parent_tail
+
 
     def debug_missing(self):
         keys = [
