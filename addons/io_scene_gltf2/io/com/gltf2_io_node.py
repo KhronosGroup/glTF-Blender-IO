fd848c9c8a736a740d75c390153e38a2bdc560bc
Julien Duroure <julien.duroure@gmail.com>
2018-08-27 21:46:06 +0200

    PyNode: list of properties

--- addons/io_scene_gltf2/io/com/gltf2_io_node.py
+++ addons/io_scene_gltf2/io/com/gltf2_io_node.py
@@ -35,9 +35,30 @@ class PyNode():
         self.json = json   # Node json
         self.gltf = gltf # Reference to global glTF instance
         self.scene = scene # Reference to scene
-        self.mesh = None
+
+
+        # glTF2.0 required properties
+        # No required !
+
+        # glTF2.0 not required properties, with default values
+        self.matrix      = [1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0]
+        self.rotation    = [0.0,0.0,0.0,1.0]
+        self.scale       = [1.0,1.0,1.0]
+        self.translation = [0.0,0.0,0.0]
+
+        # glTF2.0 not required properties
+        #TODO : note that all these properties are not managed yet
         self.camera = None
         self.children = []
+        self.skin = None #TODO
+        self.mesh = None
+        self.weights = []
+        self.name = ""
+        self.extensions = {}
+        self.extras = {}
+
+        # PyNode specific
+
         self.blender_object = ""
         self.animation = AnimationData(self, self.gltf)
         self.is_joint = False
