cd7662e86492826b6cd42a2911be09491ece9090
Julien Duroure <julien.duroure@gmail.com>
2018-09-09 18:39:48 +0200

    Manage binary data

---
+++ addons/io_scene_gltf2/io/imp/gltf2_io_binary.py
@@ -0,0 +1,121 @@
+"""
+ * ***** BEGIN GPL LICENSE BLOCK *****
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Contributor(s): Julien Duroure.
+ *
+ * ***** END GPL LICENSE BLOCK *****
+ """
+
+import struct
+
+class BinaryData():
+
+
+    @staticmethod
+    def get_binary_from_accessor(gltf, accessor_idx):
+        accessor   = gltf.data.accessors[accessor_idx]
+        bufferView = gltf.data.buffer_views[accessor.buffer_view] # TODO initialize with 0 when not present!
+        if bufferView.buffer in gltf.buffers.keys():
+            buffer = gltf.buffers[bufferView.buffer]
+        else:
+            # load buffer
+            gltf.load_buffer(bufferView.buffer)
+            buffer = gltf.buffers[bufferView.buffer]
+
+        accessor_offset   = accessor.byte_offset
+        bufferview_offset = bufferView.byte_offset
+
+        return buffer[accessor_offset+bufferview_offset:accessor_offset+bufferview_offset+bufferView.byte_length]
+
+
+
+    @staticmethod
+    def get_data_from_accessor(gltf, accessor_idx):
+        accessor   = gltf.data.accessors[accessor_idx]
+
+        bufferView = gltf.data.buffer_views[accessor.buffer_view] # TODO initialize with 0 when not present!
+        buffer_data = BinaryData.get_binary_from_accessor(gltf, accessor_idx)
+
+        fmt_char = gltf.fmt_char_dict[accessor.component_type]
+        component_size = struct.calcsize(fmt_char)
+        component_nb   = gltf.component_nb_dict[accessor.type]
+        fmt = '<' + (fmt_char * component_nb)
+        stride_ = struct.calcsize(fmt)
+        # TODO data alignment stuff
+
+        if bufferView.byte_stride:
+            stride = bufferView.byte_stride
+        else:
+            stride = stride_
+
+        data = []
+        offset = 0
+        while len(data) < accessor.count:
+            element = struct.unpack_from(fmt, buffer_data , offset)
+            data.append(element)
+            offset += stride
+
+        if accessor.sparse:
+            sparse_indices_data  = BinaryData.get_binary_from_sparse(gltf, accessor.sparse, "indices")
+            sparse_values_values = BinaryData.get_binary_from_sparse(gltf, accessor.sparse, "values", accessor.type)
+
+            # apply sparse
+            for cpt_idx, idx in enumerate(sparse_indices_data):
+                data[idx[0]] = sparse_values_values[cpt_idx]
+
+        return data
+
+    @staticmethod
+    def get_data_from_sparse(gltf, sparse, type_, type_val=None):
+        if type_ == "indices":
+            bufferView   = gltf.data.buffer_views[sparse.indices.buffer_view]
+            offset       = sparse.indices.byte_offset
+            component_nb = gltf.component_nb_dict['SCALAR']
+        elif type_ == "values":
+            bufferView   = gltf.data.buffer_views[sparse.values.buffer_view]
+            offset       = sparse.values.byte_offset
+            component_nb = gltf.component_nb_dict[type_val]
+
+
+        if bufferView.buffer in gltf.buffers.keys():
+            buffer = gltf.buffers[bufferView.buffer]
+        else:
+            # load buffer
+            gltf.load_buffer(bufferView.buffer)
+            buffer = gltf.buffers[bufferView.buffer]
+
+        bin_data =  buffer[offset:offset+bufferVview.byte_length]
+
+
+        fmt_char = gltf.fmt_char_dict[sparse.indices.component_type]
+        component_size = struct.calcsize(fmt_char)
+        fmt = '<' + (fmt_char * component_nb)
+        stride_ = struct.calcsize(fmt)
+        # TODO data alignment stuff ?
+
+        if bufferView.byte_stride:
+            stride = bufferView.byte_stride
+        else:
+            stride = stride_
+
+        offset = 0
+        while len(data) < sparse.count:
+            element = struct.unpack_from(fmt, bin_data , offset)
+            data.append(element)
+            offset += stride
+
+        return data
--- addons/io_scene_gltf2/io/imp/gltf2_io_gltf.py
+++ addons/io_scene_gltf2/io/imp/gltf2_io_gltf.py
@@ -24,15 +24,36 @@ from ..com.gltf2_io import *
 from ..com.gltf2_io_debug import *
 import logging
 import json
+import base64
+from os.path import dirname, join
 
 class glTFImporter():
 
     def __init__(self, filename, loglevel=logging.ERROR):
         self.filename = filename
+        self.buffers  = {}
 
         log = Log(loglevel)
         self.log = log.logger
         self.log_handler = log.hdlr
+
+        #TODO : merge with io_constants
+        self.fmt_char_dict = {}
+        self.fmt_char_dict[5120] = 'b' # Byte
+        self.fmt_char_dict[5121] = 'B' # Unsigned Byte
+        self.fmt_char_dict[5122] = 'h' # Short
+        self.fmt_char_dict[5123] = 'H' # Unsigned Short
+        self.fmt_char_dict[5125] = 'I' # Unsigned Int
+        self.fmt_char_dict[5126] = 'f' # Float
+
+        self.component_nb_dict = {}
+        self.component_nb_dict['SCALAR'] = 1
+        self.component_nb_dict['VEC2']   = 2
+        self.component_nb_dict['VEC3']   = 3
+        self.component_nb_dict['VEC4']   = 4
+        self.component_nb_dict['MAT2']   = 4
+        self.component_nb_dict['MAT3']   = 9
+        self.component_nb_dict['MAT4']   = 16
 
     @staticmethod
     def bad_json_value(val):
@@ -85,3 +106,19 @@ class glTFImporter():
                 return True, skin_idx
 
         return is_joint, None
+
+    def load_buffer(self, buffer_idx):
+        buffer = self.data.buffers[buffer_idx]
+
+        if buffer.uri:
+            sep = ';base64,'
+            if buffer.uri[:5] == 'data:':
+                idx = buffer.uri.find(sep)
+                if idx != -1:
+                    data = buffer.uri[idx+len(sep):]
+                    self.buffers[buffer_idx] = base64.b64decode(data)
+                    return
+
+
+            with open(join(dirname(self.filename), buffer.uri), 'rb') as f_:
+                self.buffers[buffer_idx] = f_.read()
