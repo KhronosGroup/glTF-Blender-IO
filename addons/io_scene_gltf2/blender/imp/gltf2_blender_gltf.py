c2ff16ddc39f4409089650602ac25d8a0c48c89c
Julien Duroure <julien.duroure@gmail.com>
2018-09-13 22:00:45 +0200

    Fix default value for pbr basecolorfactor

--- addons/io_scene_gltf2/blender/imp/gltf2_blender_gltf.py
+++ addons/io_scene_gltf2/blender/imp/gltf2_blender_gltf.py
@@ -98,10 +98,10 @@ class BlenderGlTF():
                         material.pbr_metallic_roughness.metallic_type = gltf.TEXTURE
 
                     if material.pbr_metallic_roughness.base_color_factor:
-                        if material.pbr_metallic_roughness.color_type == gltf.TEXTURE and material.pbr_metallic_roughness.base_color_factor != [1.0,1.0,1.0]:
+                        if material.pbr_metallic_roughness.color_type == gltf.TEXTURE and material.pbr_metallic_roughness.base_color_factor != [1.0,1.0,1.0,1.0]:
                             material.pbr_metallic_roughness.color_type = gltf.TEXTURE_FACTOR
                     else:
-                        material.pbr_metallic_roughness.base_color_factor = [1.0,1.0,1.0]
+                        material.pbr_metallic_roughness.base_color_factor = [1.0,1.0,1.0,1.0]
 
                     if material.pbr_metallic_roughness.metallic_factor:
                         if material.pbr_metallic_roughness.metallic_type == gltf.TEXTURE and material.pbr_metallic_roughness.mettalic_factor != 1.0:
