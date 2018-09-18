0e4d9d51a9f37c991c059007105df0ecc0e4060a
Urs Hanselmann <hanselmann@ux3d.io>
2018-09-18 14:22:40 +0200

    move plugin installation from testenv to test image

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker build -t blender-gltf-testenv -f Dockerfile.testenv .
-docker build -t gltf-io-test -f Dockerfile.tests tests
+docker build -t gltf-io-test -f Dockerfile.tests .
--- docker_test.sh
+++ docker_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build -t gltf-io-test -f Dockerfile.tests tests
+docker build -t gltf-io-test -f Dockerfile.tests .
 
 # TODO: find a more elegant way to do this
 docker run -t gltf-io-test npm test
