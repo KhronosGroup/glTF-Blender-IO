1296559342f40b2ae4e59b431b7cd451edec24e5
Urs Hanselmann <hanselmann@ux3d.io>
2018-09-18 10:40:46 +0200

    add node based testing framework

---
+++ docker_test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker build -t gltf-io-test -f Dockerfile.tests tests
+
+# TODO: find a more elegant way to do this
+docker run -t gltf-io-test npm test
+ID=$(docker ps -l -q)
+docker cp $ID:/tests/mochawesome-report/ . 
+docker rm $ID
