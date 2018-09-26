1b1512bbfab8023b3054f0a02a434a8236058616
Julien Duroure <julien.duroure@gmail.com>
2018-09-26 06:35:53 +0200

    fix interpolation / typo

--- addons/io_scene_gltf2/blender/imp/gltf2_blender_animation_bone.py
+++ addons/io_scene_gltf2/blender/imp/gltf2_blender_animation_bone.py
@@ -150,6 +150,6 @@ class BlenderBoneAnim():
                     bone.keyframe_insert(blender_path, frame = key[0] * fps, group='scale')
 
                 # Setting interpolation
-                for fcurve in [curve for curve in obj.animation_data.action.fcurves if curve.group.name == "rotation"]:
+                for fcurve in [curve for curve in obj.animation_data.action.fcurves if curve.group.name == "scale"]:
                     for kf in fcurve.keyframe_points:
                         BlenderBoneAnim.set_interpolation(animation.samplers[channel.sampler].interpolation, kf)
