db4df6f8f5abbed03e1e5f885abfb3633d545f23
Urs Hanselmann <hanselmann@ux3d.io>
2018-10-19 14:46:54 +0200

    add first importer/exporter roundtrip test

--- tests/roundtrip_gltf.py
+++ tests/roundtrip_gltf.py
@@ -13,13 +13,34 @@
 # limitations under the License.
 
 import bpy
+import os
 import sys
 
 try:
     argv = sys.argv
-    argv = argv[argv.index("--") + 1:]  # get all args after "--"
+    if "--" in argv:
+        argv = argv[argv.index("--") + 1:]  # get all args after "--"
+    else:
+        argv = []
+
+    filepath = argv[0]
 
     bpy.ops.wm.read_factory_settings(use_empty=True)
     bpy.ops.import_scene.gltf(filepath=argv[0])
+
+    extension = '.gltf'
+    if '--glb' in argv:
+        extension = '.glb'
+
+    path = os.path.splitext(filepath)[0] + extension
+    path_parts = os.path.split(path)
+    output_dir = os.path.join(path_parts[0], argv[1])
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    if extension == '.glb':
+        bpy.ops.export_scene.glb(filepath=os.path.join(output_dir, path_parts[1]), export_experimental=('--experimental' in argv))
+    else:
+        bpy.ops.export_scene.gltf(filepath=os.path.join(output_dir, path_parts[1]), export_experimental=('--experimental' in argv))
 except Exception as err:
+    print(err, file=sys.stderr)
     sys.exit(1)
--- tests/test/test.js
+++ tests/test/test.js
@@ -12,16 +12,33 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const fs = require('fs');
 
-// TODO: move this to environment variables
+// TODO: move glTF Validator path to environment variable
 var gltfValidatorCommand = 'dart /opt/glTF-Validator/build/bin/gltf_validator.snapshot -r -a -p';
 
 var blenderSampleScenes = ["01_alpha_blend", "01_alpha_mask", "01_color_attribute", "01_cs_morph", "01_cs_rotate", "01_cs_scale", "01_cs_translate", "01_cube", "01_cube_no_material", "01_metallic_sphere", "01_morphed_cube", "01_morphed_cube_no_uv", "01_morphed_triangle", "01_plane", "01_sphere", "01_textured_sphere", "01_triangle", "01_two_sided_plane", "02_node_hierarchy", "02_shared_mesh", "02_suzanne", "03_all_animations", "03_animated_cube", "03_skinned_cylinder", "04_common_materials", "04_lenna", "04_lights", "04_sphere_specular_glossiness", "05_metallic_sphere_light", "05_node_material", "06_parent-inverse-anim", "07_nla-anim"];
 
-function blenderFileToGltf(blenderPath, done, options='') {
+function blenderFileToGltf(blenderPath, outDirName, done, options='') {
     const { exec } = require('child_process');
-    const cmd = `blender -b --addons io_scene_gltf2 -noaudio ${blenderPath} --python export_gltf.py -- ${options}`;
+    const cmd = `blender -b --addons io_scene_gltf2 -noaudio ${blenderPath} --python export_gltf.py -- ${outDirName} ${options}`;
     var prc = exec(cmd, (error, stdout, stderr) => {
+        //if (stderr) process.stderr.write(stderr);
+
+        if (error) {
+            done(error);
+            return;
+        }
+        done();
+    });
+}
+
+function blenderRoundtripGltf(gltfPath, outDirName, done, options='') {
+    const { exec } = require('child_process');
+    const cmd = `blender -b --addons io_scene_gltf2 -noaudio --python roundtrip_gltf.py -- ${gltfPath} ${outDirName} ${options}`;
+    var prc = exec(cmd, (error, stdout, stderr) => {
+        //if (stderr) process.stderr.write(stderr);
+
         if (error) {
             done(error);
             return;
@@ -34,6 +51,9 @@ function validateGltf(gltfPath, done) {
     const { exec } = require('child_process');
     const cmd = `${gltfValidatorCommand} ${gltfPath}`;
     var prc = exec(cmd, (error, stdout, stderr) => {
+        //if (stdout) process.stdout.write(stdout);
+        //if (stderr) process.stderr.write(stderr);
+
         if (error) {
             //console.error(`exec error: ${error}`);
             done(error);
@@ -60,60 +80,87 @@ describe('General', function() {
 });
 
 describe('Exporter', function() {
-    describe('blender_export', function() {
-        blenderSampleScenes.forEach((scene) => {
-            it(scene, function(done) {
-                var blenderPath = `scenes/${scene}.blend`;
-                var gltfPath = `scenes/${scene}.gltf`;
-                blenderFileToGltf(blenderPath, (error) => {
-                    if (error)
-                        return done(error);
+    let variants = [
+        ['', ''],
+        ['_glb', '--glb'],
+        //['_experimental', '--experimental'],
+        //['_experimental_glb', '--experimental --glb']
+    ];
 
-                    validateGltf(gltfPath, done);
+    variants.forEach(function(variant) {
+        const args = variant[1];
+        describe('blender_export' + variant[0], function() {
+            blenderSampleScenes.forEach((scene) => {
+                it(scene, function(done) {
+                    let outDirName = 'out' + variant[0];
+                    let blenderPath = `scenes/${scene}.blend`;
+                    let ext = args.indexOf('--glb') === -1 ? '.gltf' : '.glb';
+                    let dstPath = `scenes/${outDirName}/${scene}${ext}`;
+                    blenderFileToGltf(blenderPath, outDirName, (error) => {
+                        if (error)
+                            return done(error);
+
+                        validateGltf(dstPath, done);
+                    }, args);
                 });
             });
         });
     });
-    describe('blender_export_glb', function() {
-        blenderSampleScenes.forEach((scene) => {
-            it(scene, function(done) {
-                var blenderPath = `scenes/${scene}.blend`;
-                var glbPath = `scenes/glb/${scene}.glb`;
-                blenderFileToGltf(blenderPath, (error) => {
-                    if (error)
-                        return done(error);
+});
 
-                    validateGltf(glbPath, done);
-                }, '--glb');
+describe('Importer / Exporter (Roundtrip)', function() {
+    let variants = [
+        ['', ''],
+        ['_glb', '--glb'],
+        //['_experimental', '--experimental'],
+        //['_experimental_glb', '--experimental --glb']
+    ];
+
+    variants.forEach(function(variant) {
+        const args = variant[1];
+        describe('blender_roundtrip' + variant[0], function() {
+            let dirs = fs.readdirSync('roundtrip');
+            dirs.forEach((dir) => {
+                if (!fs.statSync('roundtrip/' + dir).isDirectory())
+                    return;
+
+                it(dir, function(done) {
+                    let outDirName = 'out' + variant[0];
+                    let gltfSrcPath = `roundtrip/${dir}/${dir}.gltf`;
+                    let gltfSrcReport = JSON.parse(fs.readFileSync(`roundtrip/${dir}/${dir}_report.json`, 'utf8'));
+                    let ext = args.indexOf('--glb') === -1 ? '.gltf' : '.glb';
+                    let gltfDstPath = `roundtrip/${dir}/${outDirName}/${dir}${ext}`;
+                    blenderRoundtripGltf(gltfSrcPath, outDirName, (error) => {
+                        if (error)
+                            return done(error);
+
+                        validateGltf(gltfDstPath, (error) => {
+                            if (error)
+                                return done(error);
+
+                            let gltfDstReportPath = gltfDstPath.substr(0, gltfDstPath.lastIndexOf('.')) + '_report.json';
+                            let gltfDstReport = JSON.parse(fs.readFileSync(gltfDstReportPath, 'utf8'));
+
+                            const info_keys = ['version', 'hasAnimations', 'hasMaterials', 'hasMorphTargets', 'hasSkins', 'hasTextures', 'hasDefaultScene', 'primitivesCount', 'maxAttributesUsed'];
+                            let reduceKeys = function(raw, allowed) {
+                                return Object.keys(raw)
+                                  .filter(key => allowed.includes(key))
+                                  .reduce((obj, key) => {
+                                    obj[key] = raw[key];
+                                    return obj;
+                                  }, {});
+                            };
+
+                            let srcInfo = reduceKeys(gltfSrcReport.info, info_keys);
+                            let dstInfo = reduceKeys(gltfDstReport.info, info_keys);
+
+                            assert.deepStrictEqual(dstInfo, srcInfo);
+
+                            done();
+                        });
+                    }, args);
+                });
             });
         });
     });
-    /*describe('blender_export_experimental', function() {
-        blenderSampleScenes.forEach((scene) => {
-            it(scene, function(done) {
-                var blenderPath = `scenes/${scene}.blend`;
-                var gltfPath = `scenes/${scene}.gltf`;
-                blenderFileToGltf(blenderPath, (error) => {
-                    if (error)
-                        return done(error);
-
-                    validateGltf(gltfPath, done);
-                }, '--experimental');
-            });
-        });
-    });
-    describe('blender_export_experimental_glb', function() {
-        blenderSampleScenes.forEach((scene) => {
-            it(scene, function(done) {
-                var blenderPath = `scenes/${scene}.blend`;
-                var glbPath = `scenes/glb/${scene}.glb`;
-                blenderFileToGltf(blenderPath, (error) => {
-                    if (error)
-                        return done(error);
-
-                    validateGltf(glbPath, done);
-                }, '--experimental --glb');
-            });
-        });
-    });*/
 });
