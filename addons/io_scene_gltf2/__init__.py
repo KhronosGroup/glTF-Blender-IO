963c7e41d7c6570b3fdba4c109364d9f88400cf9
Julien Duroure <julien.duroure@gmail.com>
2018-11-09 22:40:20 +0100

    Add a version to bl_info. Currently set to 0.0.1

--- addons/io_scene_gltf2/__init__.py
+++ addons/io_scene_gltf2/__init__.py
@@ -41,6 +41,7 @@ from bpy_extras.io_utils import (ExportHelper)
 bl_info = {
     'name': 'glTF 2.0 format',
     'author': 'Julien Duroure, Norbert Nopper, Urs Hanselmann & Moritz Becher',
+    "version": (0, 0, 1),
     'blender': (2, 80, 0),
     'location': 'File > Import-Export',
     'description': 'Import-Export as glTF 2.0',
