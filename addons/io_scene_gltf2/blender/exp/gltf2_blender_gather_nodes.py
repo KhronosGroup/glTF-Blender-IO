bd40c28db6c251cdc4aee2d0ab0accc7f357369d
Pelle Johnsen <p.johnsen@globalscanning.com>
2019-02-06 21:39:06 +0100

    Fix variable naming

--- addons/io_scene_gltf2/blender/exp/gltf2_blender_gather_nodes.py
+++ addons/io_scene_gltf2/blender/exp/gltf2_blender_gather_nodes.py
@@ -193,8 +193,8 @@ def __gather_mesh(blender_object, export_settings):
 
         if export_settings[gltf2_blender_export_keys.SKINS]:
             # restore Armature modifiers
-            for key, val in armature_modifiers.items():
-                blender_object.modifiers[key].show_viewport = val
+            for idx, show_viewport in armature_modifiers.items():
+                blender_object.modifiers[idx].show_viewport = show_viewport
 
         if auto_smooth:
             bpy.data.objects.remove(blender_object)
