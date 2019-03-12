1c020ea97b5671a326bcdf97b183f512cc280f3b
Cyp <cyp@wz2100.net>
2019-03-12 08:53:53 +0100

    Deduplicate accessors.
    Meshes with modifiers remain duplicated, but should now at least share accessors.
    
    Doesn't help with performance due to regenerating the same mesh multiple times.

--- addons/io_scene_gltf2/io/exp/gltf2_io_binary_data.py
+++ addons/io_scene_gltf2/io/exp/gltf2_io_binary_data.py
@@ -25,6 +25,12 @@ class BinaryData:
             raise TypeError("Data is not a bytes array")
         self.data = data
 
+    def __eq__(self, other):
+        return self.data == other.data
+
+    def __hash__(self):
+        return hash(self.data)
+
     @classmethod
     def from_list(cls, lst: typing.List[typing.Any], gltf_component_type: gltf2_io_constants.ComponentType):
         format_char = gltf2_io_constants.ComponentType.to_type_code(gltf_component_type)
