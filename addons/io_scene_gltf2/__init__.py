a8e11b685daa271ed48778712d7dd0c65aa5afeb
Julien Duroure <julien.duroure@gmail.com>
2019-06-05 06:52:33 +0200

    Bump to 0.9.22

--- addons/io_scene_gltf2/__init__.py
+++ addons/io_scene_gltf2/__init__.py
@@ -15,7 +15,7 @@
 bl_info = {
     'name': 'glTF 2.0 format',
     'author': 'Julien Duroure, Norbert Nopper, Urs Hanselmann, Moritz Becher, Benjamin Schmithüsen, Jim Eckerlein, and many external contributors',
-    "version": (0, 9, 21),
+    "version": (0, 9, 22),
     'blender': (2, 80, 0),
     'location': 'File > Import-Export',
     'description': 'Import-Export as glTF 2.0',
