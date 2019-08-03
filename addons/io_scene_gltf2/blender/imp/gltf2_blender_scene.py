3e5314d4e51f7a5129fcf8bf9516221ca9e8adf5
Julien Duroure <julien.duroure@gmail.com>
2019-08-03 10:28:32 +0200

    Importer: fix setting active root object when this is an armature

--- addons/io_scene_gltf2/blender/imp/gltf2_blender_scene.py
+++ addons/io_scene_gltf2/blender/imp/gltf2_blender_scene.py
@@ -205,10 +205,14 @@ class BlenderScene():
 
         # Make first root object the new active one
         if list_nodes is not None:
-            if bpy.app.version < (2, 80, 0):
-                bpy.context.scene.objects.active = bpy.data.objects[gltf.data.nodes[list_nodes[0]].blender_object]
+            if gltf.data.nodes[list_nodes[0]].blender_object:
+                bl_name = gltf.data.nodes[list_nodes[0]].blender_object
             else:
-                bpy.context.view_layer.objects.active = bpy.data.objects[gltf.data.nodes[list_nodes[0]].blender_object]
+                bl_name = gltf.data.nodes[list_nodes[0]].blender_armature_name
+            if bpy.app.version < (2, 80, 0):
+                bpy.context.scene.objects.active = bpy.data.objects[bl_name]
+            else:
+                bpy.context.view_layer.objects.active = bpy.data.objects[bl_name]
 
     @staticmethod
     def get_root_nodes(gltf):
