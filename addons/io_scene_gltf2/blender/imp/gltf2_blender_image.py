19df5663e4f9bf6ccb02d2ce23d92dee2c120412
Julien Duroure <julien.duroure@gmail.com>
2019-09-17 22:21:45 +0200

    Remove workaround now that blender bug is fixed

--- addons/io_scene_gltf2/blender/imp/gltf2_blender_image.py
+++ addons/io_scene_gltf2/blender/imp/gltf2_blender_image.py
@@ -79,7 +79,6 @@ class BlenderImage():
                         return
 
                 blender_image = bpy.data.images.load(path)
-                blender_image.reload() # workaround bug in 2.81 alpha
                 blender_image.name = img_name
                 blender_image['tex_transform'] = {}
                 blender_image['tex_transform'][str(tex_index)] = tex_transform
@@ -104,7 +103,6 @@ class BlenderImage():
                 tmp_image.close()
 
                 blender_image = bpy.data.images.load(tmp_image.name)
-                blender_image.reload() # workaround bug in 2.81 alpha
                 blender_image.pack()
                 blender_image.name = img_name
                 blender_image['tex_transform'] = {}
