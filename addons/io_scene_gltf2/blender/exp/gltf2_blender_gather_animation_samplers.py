2364c23f0f2b4a3139f7410b635cc814210c6f7d
Julien Duroure <julien.duroure@gmail.com>
2019-12-29 20:21:21 +0100

    Fix regression. TODO for bones

--- addons/io_scene_gltf2/blender/exp/gltf2_blender_gather_animation_samplers.py
+++ addons/io_scene_gltf2/blender/exp/gltf2_blender_gather_animation_samplers.py
@@ -258,32 +258,37 @@ def __gather_interpolation(channels: typing.Tuple[bpy.types.FCurve],
                            bake_channel: typing.Union[str, None]
                            ) -> str:
 
-    max_keyframes = max([len(ch.keyframe_points) for ch in channels if ch is not None])
-    # If only single keyframe revert to STEP
-    if max_keyframes < 2:
-        return 'STEP'
-
     # Note: channels has some None items only for SK if some SK are not animated
-    blender_keyframe = [c for c in channels if c is not None][0].keyframe_points[0]
 
     if gltf2_blender_gather_animation_sampler_keyframes.needs_baking(blender_object_if_armature,
                                                                      channels,
                                                                      export_settings):
+        if bake_bone is not None:
+            # TODO: check if the bone was animated with CONSTANT
+            return 'LINEAR'
+        else:
+            max_keyframes = max([len(ch.keyframe_points) for ch in channels if ch is not None])
+            # If only single keyframe revert to STEP
+            if max_keyframes < 2:
+                return 'STEP'
+            else:
+                blender_keyframe = [c for c in channels if c is not None][0].keyframe_points[0]
 
-        # For sampled animations: CONSTANT are STEP, other are LINEAR
-        return {
-            "BEZIER": "LINEAR",
-            "LINEAR": "LINEAR",
-            "CONSTANT": "STEP"
-        }[blender_keyframe.interpolation]
+                # For sampled animations: CONSTANT are STEP, other are LINEAR
+                return {
+                    "BEZIER": "LINEAR",
+                    "LINEAR": "LINEAR",
+                    "CONSTANT": "STEP"
+                }[blender_keyframe.interpolation]
 
-    else:
-        # Select the interpolation method. Any unsupported method will fallback to STEP
-        return {
-            "BEZIER": "CUBICSPLINE",
-            "LINEAR": "LINEAR",
-            "CONSTANT": "STEP"
-        }[blender_keyframe.interpolation]
+    blender_keyframe = [c for c in channels if c is not None][0].keyframe_points[0]
+
+    # Select the interpolation method. Any unsupported method will fallback to STEP
+    return {
+        "BEZIER": "CUBICSPLINE",
+        "LINEAR": "LINEAR",
+        "CONSTANT": "STEP"
+    }[blender_keyframe.interpolation]
 
 
 @cached
