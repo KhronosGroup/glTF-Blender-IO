1b7ebe7e38ee03f269ca87ac8ee988a1a39106f1
Julien Duroure <julien.duroure@gmail.com>
2020-03-04 22:46:26 +0100

    Check mode before going to OBJECT.
    For linked object, already in object mode, you can set mode

--- addons/io_scene_gltf2/blender/exp/gltf2_blender_export.py
+++ addons/io_scene_gltf2/blender/exp/gltf2_blender_export.py
@@ -29,7 +29,8 @@ from io_scene_gltf2.io.exp import gltf2_io_draco_compression_extension
 def save(context, export_settings):
     """Start the glTF 2.0 export and saves to content either to a .gltf or .glb file."""
     if bpy.context.active_object is not None:
-        bpy.ops.object.mode_set(mode='OBJECT')
+        if bpy.context.active_object.mode != "OBJECT":
+            bpy.ops.object.mode_set(mode='OBJECT')
 
     original_frame = bpy.context.scene.frame_current
     if not export_settings['gltf_current_frame']:
