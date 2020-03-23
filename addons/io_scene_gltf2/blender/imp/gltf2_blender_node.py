86ad0501c2bd60862b3f9975ae1ff4c757b286c2
scurest <scurest@users.noreply.github.com>
2020-03-22 22:44:53 -0500

    Import/Refactor: setup vertex groups/modifiers during node creation
    Since the arma and bones for a skinned mesh are always created
    before it, we don't need to defer creating these to a separate pass
    anymore. Just do it when we create the mesh object now.

--- addons/io_scene_gltf2/blender/imp/gltf2_blender_node.py
+++ addons/io_scene_gltf2/blender/imp/gltf2_blender_node.py
@@ -205,6 +205,9 @@ class BlenderNode():
         if pymesh.shapekey_names:
             BlenderNode.set_morph_weights(gltf, pynode, obj)
 
+        if pynode.skin is not None:
+            BlenderNode.setup_skinning(gltf, pynode, obj)
+
         return obj
 
     @staticmethod
@@ -214,3 +217,20 @@ class BlenderNode():
         for i, weight in enumerate(weights):
             if pymesh.shapekey_names[i] is not None:
                 obj.data.shape_keys.key_blocks[pymesh.shapekey_names[i]].value = weight
+
+    @staticmethod
+    def setup_skinning(gltf, pynode, obj):
+        pyskin = gltf.data.skins[pynode.skin]
+
+        # Armature/bones should have already been created.
+
+        # Create vertex groups for each joint
+        for node_idx in pyskin.joints:
+            bone = gltf.vnodes[node_idx]
+            obj.vertex_groups.new(name=bone.blender_bone_name)
+
+        # Create an Armature modifier
+        first_bone = gltf.vnodes[pyskin.joints[0]]
+        arma = gltf.vnodes[first_bone.bone_arma]
+        mod = obj.modifiers.new(name="Armature", type="ARMATURE")
+        mod.object = arma.blender_object
