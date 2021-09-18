0accadf10515fd0c40b8d3b4261800d722b5ceed
Julien Duroure <julien.duroure@gmail.com>
2021-09-18 11:15:15 +0200

    WIP - base of importer user extension

--- addons/io_scene_gltf2/io/imp/gltf2_io_gltf.py
+++ addons/io_scene_gltf2/io/imp/gltf2_io_gltf.py
@@ -38,6 +38,7 @@ class glTFImporter():
         self.buffers = {}
         self.accessor_cache = {}
         self.decode_accessor_cache = {}
+        self.import_user_extensions = import_settings['import_user_extensions']
 
         if 'loglevel' not in self.import_settings.keys():
             self.import_settings['loglevel'] = logging.ERROR
