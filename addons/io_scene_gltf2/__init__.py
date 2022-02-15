17c1e5f05b7bcaa69cbde76dba9689c4582bc8a4
Julien Duroure <julien.duroure@gmail.com>
2022-02-15 16:52:37 +0100

    Fix UI message

--- addons/io_scene_gltf2/__init__.py
+++ addons/io_scene_gltf2/__init__.py
@@ -161,7 +161,7 @@ class ExportGLTF2_Base:
                 'Save images as JPEGs. (Images that need alpha are saved as PNGs though.) '
                 'Be aware of a possible loss in quality'),
                 ('NONE', 'None',
-                 'Don\'t export images.'),
+                 'Don\'t export images'),
                ),
         description=(
             'Output format for images. PNG is lossless and generally preferred, but JPEG might be preferable for web '
