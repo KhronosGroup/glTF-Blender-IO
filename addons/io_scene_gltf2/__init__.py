e3300e742e647c9a3dd6fbdf5fe37ccea072c8c8
weckr <markalarm@gmail.com>
2022-02-18 18:04:36 +0100

    renamed to optimize animation state

--- addons/io_scene_gltf2/__init__.py
+++ addons/io_scene_gltf2/__init__.py
@@ -388,6 +388,15 @@ class ExportGLTF2_Base:
         default=False
     )
 
+    optimize_animation_size: BoolProperty(
+        name='Optimize Animation Size',
+        description=(
+            "Reduces exported filesize by removing duplicate keyframes"
+            "Can cause problems with stepped animation"
+        ),
+        default=True
+    )
+
     export_current_frame: BoolProperty(
         name='Use Current Frame',
         description='Export the scene in the current animation frame',
@@ -580,11 +589,13 @@ class ExportGLTF2_Base:
             else:
                 export_settings['gltf_def_bones'] = False
             export_settings['gltf_nla_strips'] = self.export_nla_strips
+            export_settings['gltf_optimize_animation'] = self.optimize_animation_size
         else:
             export_settings['gltf_frame_range'] = False
             export_settings['gltf_move_keyframes'] = False
             export_settings['gltf_force_sampling'] = False
             export_settings['gltf_def_bones'] = False
+            export_settings['gltf_optimize_animation'] = False
         export_settings['gltf_skins'] = self.export_skins
         if self.export_skins:
             export_settings['gltf_all_vertex_influences'] = self.export_all_influences
@@ -872,6 +883,7 @@ class GLTF_PT_export_animation_export(bpy.types.Panel):
         layout.prop(operator, 'export_frame_step')
         layout.prop(operator, 'export_force_sampling')
         layout.prop(operator, 'export_nla_strips')
+        layout.prop(operator, 'optimize_animation_size')
 
         row = layout.row()
         row.active = operator.export_force_sampling
