12eab6e8fc748de03b0dc4061ac43863e72b65d6
Julien Duroure <julien.duroure@gmail.com>
2022-05-19 09:24:46 +0200

    add TODO

--- addons/io_scene_gltf2/blender/com/gltf2_blender_ui.py
+++ addons/io_scene_gltf2/blender/com/gltf2_blender_ui.py
@@ -59,6 +59,8 @@ def add_gltf_settings_to_menu(self, context) :
 
 # Global UI panel
 
+# TODOVariant : Global assignment for entire mesh, not slot by slot
+
 class gltf2_KHR_materials_variants_variant(bpy.types.PropertyGroup):
     variant_idx : bpy.props.IntProperty()
     name : bpy.props.StringProperty(name="Variant Name")
