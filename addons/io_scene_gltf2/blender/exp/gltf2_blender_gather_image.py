0a4abfc7c7e13c505cc968a505fd09d7111f8d3b
Julien Duroure <julien.duroure@gmail.com>
2022-06-19 17:12:29 +0200

    Fix exporting keeping original texture

--- addons/io_scene_gltf2/blender/exp/gltf2_blender_gather_image.py
+++ addons/io_scene_gltf2/blender/exp/gltf2_blender_gather_image.py
@@ -51,6 +51,7 @@ def gather_image(
         uri = __gather_original_uri(image_data.original.filepath, export_settings)
         # In case we can't retrieve image (for example packed images, with original moved)
         # We don't create invalid image without uri
+        factor_uri = None
         if uri is None: return None
 
     buffer_view, factor_buffer_view = __gather_buffer_view(image_data, mime_type, name, export_settings)
