6dc1e4e1e8544a162f6112df95e4c55a1faf996f
Julien Duroure <julien.duroure@gmail.com>
2022-08-05 21:49:51 +0200

    Merge all custom socket into a single node

--- addons/io_scene_gltf2/blender/com/gltf2_blender_material_helpers.py
+++ addons/io_scene_gltf2/blender/com/gltf2_blender_material_helpers.py
@@ -22,23 +22,11 @@ def create_settings_group(name):
     gltf_node_group.inputs.new("NodeSocketFloat", "Occlusion")
     thicknessFactor  = gltf_node_group.inputs.new("NodeSocketFloat", "Thickness")
     thicknessFactor.default_value = 1.0
-    gltf_node_group.nodes.new('NodeGroupOutput')
-    gltf_node_group_input = gltf_node_group.nodes.new('NodeGroupInput')
-    gltf_node_group_input.location = -200, 0
-    return gltf_node_group
-
-def get_gltf_pbr_non_converted_name():
-    return "original glTF PBR data"
-
-def create_gltf_pbr_non_converted_group(name):
-    gltf_node_group = bpy.data.node_groups.new(name, 'ShaderNodeTree')
-
     specular = gltf_node_group.inputs.new("NodeSocketFloat", "specular glTF")
     specular.default_value = 1.0
     specularColor = gltf_node_group.inputs.new("NodeSocketColor", "specularColor glTF")
     specularColor.default_value = [1.0,1.0,1.0,1.0]
-
     gltf_node_group.nodes.new('NodeGroupOutput')
     gltf_node_group_input = gltf_node_group.nodes.new('NodeGroupInput')
-    gltf_node_group_input.location = -400, 0
-    return gltf_node_group    
+    gltf_node_group_input.location = -200, 0
+    return gltf_node_group
