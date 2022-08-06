e207340bf023e3a5298eeda7155fe7a85b06b02e
Julien Duroure <julien.duroure@gmail.com>
2022-08-06 15:38:08 +0200

    Fix bad merge

--- addons/io_scene_gltf2/blender/exp/gltf2_blender_get.py
+++ addons/io_scene_gltf2/blender/exp/gltf2_blender_get.py
@@ -15,7 +15,7 @@
 import bpy
 from mathutils import Vector, Matrix
 
-from ..com.gltf2_blender_material_helpers import get_gltf_node_name, get_gltf_node_old_name, get_gltf_pbr_non_converted_name
+from ..com.gltf2_blender_material_helpers import get_gltf_node_name, get_gltf_node_old_name
 from ...blender.com.gltf2_blender_conversion import texture_transform_blender_to_gltf
 from io_scene_gltf2.io.com import gltf2_io_debug
 from io_scene_gltf2.blender.exp import gltf2_blender_search_node_tree
