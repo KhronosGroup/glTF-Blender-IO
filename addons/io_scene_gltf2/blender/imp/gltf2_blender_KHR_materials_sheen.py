b31114ae2dcad669b6704da47cdd07af468da99d
Julien Duroure <julien.duroure@gmail.com>
2022-12-10 00:18:42 +0100

    Fix factors after Mix RGB / Mix  after Blender change

--- addons/io_scene_gltf2/blender/imp/gltf2_blender_KHR_materials_sheen.py
+++ addons/io_scene_gltf2/blender/imp/gltf2_blender_KHR_materials_sheen.py
@@ -51,16 +51,17 @@ def sheen(  mh,
         # Mix sheenColor factor
         sheenColorFactor = sheenColorFactor + [1.0]
         if sheenColorFactor != [1.0, 1.0, 1.0, 1.0]:
-            node = mh.node_tree.nodes.new('ShaderNodeMixRGB')
+            node = mh.node_tree.nodes.new('ShaderNodeMix')
             node.label = 'sheenColor Factor'
+            node.data_type = 'RGBA'
             node.location = x_sheenColor - 140, y_sheenColor
             node.blend_type = 'MULTIPLY'
             # Outputs
             mh.node_tree.links.new(sheenColor_socket, node.outputs[0])
             # Inputs
-            node.inputs['Fac'].default_value = 1.0
-            sheenColor_socket = node.inputs['Color1']
-            node.inputs['Color2'].default_value = sheenColorFactor
+            node.inputs['Factor'].default_value = 1.0
+            sheenColor_socket = node.inputs[6]
+            node.inputs[7].default_value = sheenColorFactor
             x_sheenColor -= 200
 
         texture(
