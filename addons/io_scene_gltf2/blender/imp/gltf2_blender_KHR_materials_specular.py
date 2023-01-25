ee0f2d6741d8c771d9960070abc13749d0882e27
Julien Duroure <julien.duroure@gmail.com>
2023-01-25 17:19:31 +0100

    Avoid division by zero in specular when transmission is 1.0

--- addons/io_scene_gltf2/blender/imp/gltf2_blender_KHR_materials_specular.py
+++ addons/io_scene_gltf2/blender/imp/gltf2_blender_KHR_materials_specular.py
@@ -124,7 +124,11 @@ def specular(mh, location_specular,
 
         f0_from_ior = ((ior - 1)/(ior + 1))**2
         lum_specular_color = luminance(specular_color_factor)
-        blender_specular = ((lum_specular_color - transmission_factor) / (1 - transmission_factor)) * (1 / 0.08) * f0_from_ior
+        if transmission_factor != 1.0:
+            blender_specular = ((lum_specular_color - transmission_factor) / (1 - transmission_factor)) * (1 / 0.08) * f0_from_ior
+        else:
+            # Avoid division by 0
+            blender_specular = 1.0
         if not all([i == 0 for i in normalize(base_color) - 1]):
             blender_specular_tint = luminance((normalize(specular_color_factor) - 1) / (normalize(base_color) - 1))
             if blender_specular_tint < 0 or blender_specular_tint > 1:
@@ -224,7 +228,11 @@ def specular(mh, location_specular,
 
         f0_from_ior = ((ior - 1)/(ior + 1))**2
         lum_specular_color = stack3(luminance(buffers['speccolor']))
-        blender_specular = ((lum_specular_color - buffers['transmission']) / (1 - buffers['transmission'])) * (1 / 0.08) * f0_from_ior
+        if transmission_factor != 1.0:
+            blender_specular = ((lum_specular_color - buffers['transmission']) / (1 - buffers['transmission'])) * (1 / 0.08) * f0_from_ior
+        else:
+            # Avoid division by 0
+            blender_specular = np.full((width, height, 3), 1.0)
         if not np.all(normalize(buffers['basecolor']) - 1 == 0.0):
             blender_specular_tint = luminance((normalize(buffers['speccolor']) - 1) / (normalize(buffers['basecolor']) - 1))
             np.nan_to_num(blender_specular_tint, copy=False)
