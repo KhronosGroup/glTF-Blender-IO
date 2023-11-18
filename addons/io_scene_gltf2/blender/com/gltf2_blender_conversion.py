d844e67bdd0cd187a01f06748031c84418c04b5e
Julien Duroure <julien.duroure@gmail.com>
2023-11-18 17:10:33 +0100

    Avoid crash when custom property type is not managed

--- addons/io_scene_gltf2/blender/com/gltf2_blender_conversion.py
+++ addons/io_scene_gltf2/blender/com/gltf2_blender_conversion.py
@@ -131,21 +131,21 @@ def get_attribute_type(component_type, data_type):
         return {
             gltf2_io_constants.ComponentType.Float: "FLOAT",
             gltf2_io_constants.ComponentType.UnsignedByte: "INT" # What is the best for compatibility?
-        }[component_type]
+        }.get(component_type, None)
     elif gltf2_io_constants.DataType.num_elements(data_type) == 2:
         return {
             gltf2_io_constants.ComponentType.Float: "FLOAT2"
-        }[component_type]
+        }.get(component_type, None)
     elif gltf2_io_constants.DataType.num_elements(data_type) == 3:
         return {
             gltf2_io_constants.ComponentType.Float: "FLOAT_VECTOR"
-        }[component_type]
+        }.get(component_type, None)
     elif gltf2_io_constants.DataType.num_elements(data_type) == 4:
         return {
             gltf2_io_constants.ComponentType.Float: "FLOAT_COLOR",
             gltf2_io_constants.ComponentType.UnsignedShort: "BYTE_COLOR",
             gltf2_io_constants.ComponentType.UnsignedByte: "BYTE_COLOR" # What is the best for compatibility?
-        }[component_type]
+        }.get(component_type, None)
     else:
         pass
 
