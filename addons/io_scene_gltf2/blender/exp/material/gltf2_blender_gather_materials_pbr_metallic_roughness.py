34e81954babeb65bcb039260aa7e99e0a1902a16
scurest <scurest@users.noreply.github.com>
2024-03-10 07:15:07 -0500

    Export: use nodes for Alpha clipping

--- addons/io_scene_gltf2/blender/exp/material/gltf2_blender_gather_materials_pbr_metallic_roughness.py
+++ addons/io_scene_gltf2/blender/exp/material/gltf2_blender_gather_materials_pbr_metallic_roughness.py
@@ -25,7 +25,8 @@ from .gltf2_blender_search_node_tree import \
     has_image_node_from_socket, \
     get_const_from_default_value_socket, \
     get_socket, \
-    get_factor_from_socket
+    get_factor_from_socket, \
+    gather_alpha_info
 
 @cached
 def gather_material_pbr_metallic_roughness(blender_material, orm_texture, export_settings):
@@ -71,10 +72,8 @@ def __gather_base_color_factor(blender_material, export_settings):
 
     alpha_socket = get_socket(blender_material, "Alpha")
     if isinstance(alpha_socket.socket, bpy.types.NodeSocket):
-        if export_settings['gltf_image_format'] != "NONE":
-            alpha = get_factor_from_socket(alpha_socket, kind='VALUE')
-        else:
-            alpha = get_const_from_default_value_socket(alpha_socket, kind='VALUE')
+        alpha_info = gather_alpha_info(alpha_socket.to_node_nav())
+        alpha = alpha_info['alphaFactor']
 
     base_color_socket = get_socket(blender_material, "Base Color")
     if base_color_socket.socket is None:
