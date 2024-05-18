645fd10177f039366b7900bd156a0693fcc54467
Julien Duroure <julien.duroure@gmail.com>
2024-05-18 10:40:51 +0200

    Fix #2110 - CV + ColorFactor

--- addons/io_scene_gltf2/blender/exp/material/gltf2_blender_search_node_tree.py
+++ addons/io_scene_gltf2/blender/exp/material/gltf2_blender_search_node_tree.py
@@ -381,6 +381,54 @@ class NodeNav:
         return None, None
 
 
+# Gather information about factor and vertex color from the Color socket
+# The general form for color is
+#   color = factor * color attribute * texture
+def gather_color_info(color_nav):
+    info = {
+        'colorFactor': None,
+        'colorAttrib': None,
+        'colorAttribType': None,
+        'colorPath': None,
+    }
+
+    # Reads the factor and color attribute by checking for variations on
+    # -> [Multiply by Factor] -> [Multiply by Color Attrib] ->
+
+    for _ in range(2):  # Twice, to handle both factor and attrib
+        # No factor found yet?
+        if info['colorFactor'] is None:
+            a, color_path = color_nav.get_constant()
+            if a is not None:
+                info['colorFactor'] = a[:3]
+                info['colorPath'] = color_path
+                break
+
+            a, color_path = detect_multiply_by_constant(color_nav)
+            if a is not None:
+                info['colorFactor'] = a[:3]
+                info['colorPath'] = color_path
+                continue
+
+        # No color attrib found yet?
+        if info['colorAttrib'] is None:
+            attr = get_color_attrib(color_nav)
+            if attr is not None:
+                info['colorAttrib'] = attr
+                info['colorAttribType'] = 'active' if attr == "" else 'name'
+                break
+
+            attr = detect_multiply_by_color_attrib(color_nav)
+            if attr is not None:
+                info['colorAttrib'] = attr
+                info['colorAttribType'] = 'active' if attr == "" else 'name'
+                continue
+
+        break
+
+    return info
+
+
 # Gather information about alpha from the Alpha socket. Alpha has the
 # general form
 #
@@ -394,6 +442,7 @@ def gather_alpha_info(alpha_nav):
         'alphaCutoff': None,
         'alphaFactor': None,
         'alphaColorAttrib': None,
+        'alphaColorAttribType': None,
         'alphaPath': None,
     }
     if not alpha_nav:
@@ -434,11 +483,13 @@ def gather_alpha_info(alpha_nav):
             attr = get_color_attrib(alpha_nav)
             if attr is not None:
                 info['alphaColorAttrib'] = attr
+                info['alphaColorAttribType'] = 'active' if attr == "" else 'name'
                 break
 
             attr = detect_multiply_by_color_attrib(alpha_nav)
             if attr is not None:
                 info['alphaColorAttrib'] = attr
+                info['alphaColorAttribType'] = 'active' if attr == "" else 'name'
                 continue
 
         break
@@ -510,7 +561,7 @@ def get_multiply_factors(nav):
                 prev.node.type == 'MIX' and
                 prev.node.data_type == 'RGBA' and
                 prev.node.blend_type == 'MULTIPLY' and
-                prev.get_constant('Fac') == 1
+                prev.get_constant('Factor')[0] == 1
             )
             if is_mul:
                 fac1 = prev
