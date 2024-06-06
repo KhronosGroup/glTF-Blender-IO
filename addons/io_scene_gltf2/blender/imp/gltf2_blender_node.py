0bd223159442e32e2f705a992cb413b3ba972949
Julien Duroure <julien.duroure@gmail.com>
2024-06-06 16:20:35 +0200

    Import Bone scale / shape option

--- addons/io_scene_gltf2/blender/imp/gltf2_blender_node.py
+++ addons/io_scene_gltf2/blender/imp/gltf2_blender_node.py
@@ -237,10 +237,10 @@ class BlenderNode():
                 pynode = gltf.data.nodes[id]
                 set_extras(pose_bone, pynode.extras)
 
-            if gltf.import_settings['bone_heuristic'] == "BLENDER":
+            if gltf.import_settings['bone_heuristic'] == "BLENDER" and gltf.import_settings['disable_bone_shape'] is False:
                 pose_bone.custom_shape = bpy.data.objects[gltf.bone_shape]
                 armature_min_dim = min([blender_arma.dimensions[0] / blender_arma.scale[0], blender_arma.dimensions[1]  / blender_arma.scale[1], blender_arma.dimensions[2] / blender_arma.scale[2]])
-                pose_bone.custom_shape_scale_xyz = Vector([armature_min_dim * 0.05] * 3)
+                pose_bone.custom_shape_scale_xyz = Vector([armature_min_dim * 0.05] * 3) * gltf.import_settings['bone_shape_scale_factor']
                 pose_bone.use_custom_shape_bone_size = False
 
     @staticmethod
