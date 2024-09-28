5f99a43fb0bf7faeaaeaaf9e19a649a650be6972
Julien Duroure <julien.duroure@gmail.com>
2024-09-28 14:29:27 +0200

    Manage alpha when performing RGB2BW

--- addons/io_scene_gltf2/blender/exp/material/encode_image.py
+++ addons/io_scene_gltf2/blender/exp/material/encode_image.py
@@ -342,8 +342,9 @@ class ExportImage:
                 elif isinstance(fill, FillWith):
                     out_buf[int(dst_chan)::4] = fill.value
                 elif isinstance(fill, FillImageRGB2BW) and fill.image == image:
-                    # TODO : alpha multiply shoud happen depending on alpha mode of the image node
-                    out_buf[int(dst_chan)::4] = (tmp_buf[0::4] * 0.2989 + tmp_buf[1::4] * 0.5870 + tmp_buf[2::4] * 0.1140) * tmp_buf[3::4]
+                    out_buf[int(dst_chan)::4] = tmp_buf[0::4] * 0.2989 + tmp_buf[1::4] * 0.5870 + tmp_buf[2::4] * 0.1140
+                    if image.alpha_mode in ["STRAIGHT", "PREMUL"]:
+                        out_buf[int(dst_chan)::4] *= tmp_buf[3::4]
 
         tmp_buf = None  # GC this
 
