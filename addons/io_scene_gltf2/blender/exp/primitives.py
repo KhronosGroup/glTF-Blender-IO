ea9230b752d1f5e0533aad926d506c4e6c8a2871
Julien Duroure <julien.duroure@gmail.com>
2024-10-06 10:32:07 +0200

    Make KHR_materials_variants not required extension

--- addons/io_scene_gltf2/blender/exp/primitives.py
+++ addons/io_scene_gltf2/blender/exp/primitives.py
@@ -304,7 +304,8 @@ def __gather_extensions(blender_mesh,
                 variant_extension = gltf2_io_extensions.ChildOfRootExtension(
                 name="KHR_materials_variants",
                 path=["variants"],
-                extension=vari
+                extension=vari,
+                required=False
             )
             variants.append(variant_extension)
         if len(variants) > 0:
@@ -331,7 +332,8 @@ def __gather_extensions(blender_mesh,
             name="KHR_materials_variants",
             extension={
                 "mappings": mapping
-            }
+            },
+            required=False
         )
 
     return extensions if extensions else None
