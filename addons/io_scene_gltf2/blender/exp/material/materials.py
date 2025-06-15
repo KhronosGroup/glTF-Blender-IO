d3234b7ef905b34a7ab1d2fdd1e76b412bf48f16
Julien Duroure <julien.duroure@gmail.com>
2025-06-15 13:05:24 +0200

    Fix double function call

--- addons/io_scene_gltf2/blender/exp/material/materials.py
+++ addons/io_scene_gltf2/blender/exp/material/materials.py
@@ -94,7 +94,7 @@ def gather_material(blender_material, export_settings):
     normal_texture, uvmap_info_normal, udim_info_normal = __gather_normal_texture(blender_material, export_settings)
     occlusion_texture, uvmap_info_occlusion, udim_occlusion = __gather_occlusion_texture(
         blender_material, orm_texture, export_settings)
-    pbr_metallic_roughness, uvmap_info_pbr_metallic_roughness, vc_info, udim_info_prb_mr, alpha_info = __gather_pbr_metallic_roughness(
+    pbr_metallic_roughness, uvmap_info_pbr_metallic_roughness, vc_info, udim_info_prb_mr, alpha_info, alpha_info = __gather_pbr_metallic_roughness(
         blender_material, orm_texture, export_settings)
 
     if any([i > 1.0 for i in emissive_factor or []]) is True:
@@ -103,7 +103,7 @@ def gather_material(blender_material, export_settings):
         emissive_factor = [f / emission_strength for f in emissive_factor]
 
     #TODOALPHAPOINTER: why call this again? This is already done in gather_material_pbr_metallic_roughness
-    alpha_socket = get_socket(blender_material.node_tree, "Alpha")
+    alpha_socket = get_socket(blender_material.node_tree, blender_material.use_nodes, "Alpha")
     if isinstance(alpha_socket.socket, bpy.types.NodeSocket):
         alpha_info = gather_alpha_info(alpha_socket.to_node_nav())
     else:
