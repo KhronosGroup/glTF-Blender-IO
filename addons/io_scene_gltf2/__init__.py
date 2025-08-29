56847f239fd5828a114505e9b837a5637df660ce
Julien Duroure <julien.duroure@gmail.com>
2025-08-29 16:37:44 +0200

    Change from Blender repo

--- addons/io_scene_gltf2/__init__.py
+++ addons/io_scene_gltf2/__init__.py
@@ -2125,6 +2125,8 @@ def import_panel_user_extension(context, layout):
 
 
 class GLTF2_filter_action(bpy.types.PropertyGroup):
+    __slots__ = ()
+
     keep: bpy.props.BoolProperty(name="Keep Animation")
     action: bpy.props.PointerProperty(type=bpy.types.Action)
 
