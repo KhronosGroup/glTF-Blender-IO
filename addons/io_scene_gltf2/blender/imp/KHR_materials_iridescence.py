45809ff165678607c14f0c587f40a8a300a727fd
Julien Duroure <julien.duroure@gmail.com>
2026-06-24 17:19:59 +0200

    Fix import iridescence Thickness Texture transform

--- addons/io_scene_gltf2/blender/imp/KHR_materials_iridescence.py
+++ addons/io_scene_gltf2/blender/imp/KHR_materials_iridescence.py
@@ -111,7 +111,7 @@ def iridescence(
         # Texture node
         texture(
             mh,
-            tex_info=tex_info,
+            tex_info=thickness_tex_info,
             label='IRIDESCENCE THICKNESS',
             location=(x - 180 * 3, y - 600),
             is_data=True,
@@ -121,9 +121,9 @@ def iridescence(
         if len(ext) > 0:
             # Because extensions are dict, they are not passed by reference
             # So we need to update the dict of the KHR_texture_transform extension if needed
-            if tex_info.extensions is not None and "KHR_texture_transform" in tex_info.extensions:
+            if thickness_tex_info.extensions is not None and "KHR_texture_transform" in thickness_tex_info.extensions:
                 mh.pymat.extensions['KHR_materials_iridescence']['iridescenceThicknessTexture'][
-                    'extensions']['KHR_texture_transform'] = tex_info.extensions["KHR_texture_transform"]
+                    'extensions']['KHR_texture_transform'] = thickness_tex_info.extensions["KHR_texture_transform"]
 
         # Value node (minimum thickness)
         value_node = mh.node_tree.nodes.new('ShaderNodeValue')
