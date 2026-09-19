4d56b0d47c3d6e236446fa9cc12859ba30e11c55
Xury Greer <Xury.Greer@Gmail.com>
2026-09-19 11:34:38 -0700

    Cleanup: Spelling in attribute name in `VExportTree` class
    Fixes the following misspellings:
    
    "tree_troncated" -> "tree_truncated"

--- addons/io_scene_gltf2/blender/exp/tree.py
+++ addons/io_scene_gltf2/blender/exp/tree.py
@@ -138,7 +138,7 @@ class VExportTree:
 
         self.export_settings = export_settings
 
-        self.tree_troncated = False
+        self.tree_truncated = False
 
         self.axis_basis_change = Matrix.Identity(4)
         if self.export_settings['gltf_yup']:
@@ -738,12 +738,12 @@ class VExportTree:
 
             # If parent_uuid is not parent_kept_uuid, we need to modify children list of parent_kept_uuid
             if parent_kept_uuid != self.nodes[uuid].parent_uuid and parent_kept_uuid is not None:
-                self.tree_troncated = True
+                self.tree_truncated = True
                 self.nodes[parent_kept_uuid].children.append(uuid)
 
             # If parent_kept_uuid is None, and parent_uuid was not, add to root list
             if self.nodes[uuid].parent_uuid is not None and parent_kept_uuid is None:
-                self.tree_troncated = True
+                self.tree_truncated = True
                 self.roots.append(uuid)
 
             # Modify parent uuid
