8ae5900bba3a17a716c557f33683d17d7256baaf
Julien Duroure <julien.duroure@gmail.com>
2026-09-26 19:13:06 +0200

    Fix coefficient conversion

--- addons/io_scene_gltf2/blender/imp/gsplat.py
+++ addons/io_scene_gltf2/blender/imp/gsplat.py
@@ -23,33 +23,33 @@ SQRT15 = np.sqrt(15)
 # Coefficient conversion from Yup to Zup
 
 def convert_sh_d1_batch(c0, c1, c2):
-    c0, c1 = c0.copy(), c1.copy()
-    c0[:] = c1
-    c1[:] = -c0
+    o0, o1 = c0.copy(), c1.copy()
+    new_c0 = o1
+    new_c1 = -o0
     # No change for c2
-    return c0, c1, c2
+    return new_c0, new_c1, c2
 
 
 def convert_sh_d2_batch(c0, c1, c2, c3, c4):
-    c0, c2, c3, c4 = c0.copy(), c2.copy(), c3.copy(), c4.copy()
-    c0[:] = c3
-    c1 *= -1
-    c2[:] = -0.5 * c2 - (SQRT3 / 2) * c4
-    c3[:] = -c0
-    c4[:] = -(SQRT3 / 2) * c2 + 0.5 * c4
-    return c0, c1, c2, c3, c4
+    o0, o2, o3, o4 = c0.copy(), c2.copy(), c3.copy(), c4.copy()
+    new_c0 = o3
+    new_c1 = -c1
+    new_c2 = -0.5 * o2 - (SQRT3 / 2) * o4
+    new_c3 = -o0
+    new_c4 = -(SQRT3 / 2) * o2 + 0.5 * o4
+    return new_c0, new_c1, new_c2, new_c3, new_c4
 
 
 def convert_sh_d3_batch(c0, c1, c2, c3, c4, c5, c6):
-    c0, c2, c3, c4, c5, c6 = (
+    o0, o2, o3, o4, o5, o6 = (
         c0.copy(), c2.copy(), c3.copy(),
         c4.copy(), c5.copy(), c6.copy(),
     )
-    c0[:] = -(SQRT10 / 4) * c3 + (SQRT6 / 4) * c5
-    c1 *= -1
-    c2[:] = -(SQRT6 / 4) * c3 - (SQRT10 / 4) * c5
-    c3[:] = (SQRT6 / 4) * c2 + (SQRT10 / 4) * c0
-    c4[:] = -0.25 * c4 - (SQRT15 / 4) * c6
-    c5[:] = (SQRT10 / 4) * c2 - (SQRT6 / 4) * c0
-    c6[:] = -(SQRT15 / 4) * c4 + 0.25 * c6
-    return c0, c1, c2, c3, c4, c5, c6
+    new_c0 = -(SQRT10 / 4) * o3 + (SQRT6 / 4) * o5
+    new_c1 = -c1
+    new_c2 = -(SQRT6 / 4) * o3 - (SQRT10 / 4) * o5
+    new_c3 = (SQRT6 / 4) * o2 + (SQRT10 / 4) * o0
+    new_c4 = -0.25 * o4 - (SQRT15 / 4) * o6
+    new_c5 = (SQRT10 / 4) * o2 - (SQRT6 / 4) * o0
+    new_c6 = -(SQRT15 / 4) * o4 + 0.25 * o6
+    return new_c0, new_c1, new_c2, new_c3, new_c4, new_c5, new_c6
